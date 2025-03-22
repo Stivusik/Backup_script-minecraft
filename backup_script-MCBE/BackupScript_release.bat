4b3ee787f29b19010e95a8765bc672854f621bcd
Stivusik <ferfersilver@gmail.com>
2025-03-23 00:20:10 +0200

    Create BackupScript_release.bat

---
+++ backup_script-MCBE/BackupScript_release.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value') do set datetime=%%i
+set "year=!datetime:~0,4!"
+set "month=!datetime:~4,2!"
+set "day=!datetime:~6,2!"
+set "hour=!datetime:~8,2!"
+set "minute=!datetime:~10,2!"
+set "second=!datetime:~12,2!"
+set "currentDate=!day!-!month!-!year!_!hour!-!minute!-!second!"
+set "source=%USERPROFILE%\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe"
+set "destination=%~dp0"
+set "folderName=Microsoft.MinecraftUWP_!currentDate!"
+xcopy "%source%" "%destination%!folderName!" /e /i /y /h
+if exist "%~dp0\7z.exe" ("%~dp0\7z.exe" a -tzip "%destination%!folderName!.zip" "%destination%!folderName!\*")
