d727dec89a275c4a6dd7f097a36db0afa057f68f
Stivusik <ferfersilver@gmail.com>
2025-03-23 00:29:55 +0200

    Create BackupScript_mods.bat

---
+++ Backup_scripts-MC_Java/backup_script-mods/BackupScript_mods.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enabledelayedexpansion
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value') do set datetime=%%i
+set "year=!datetime:~0,4!"
+set "month=!datetime:~4,2!"
+set "day=!datetime:~6,2!"
+set "hour=!datetime:~8,2!"
+set "minute=!datetime:~10,2!"
+set "second=!datetime:~12,2!"
+set "currentDate=!day!-!month!-!year!_!hour!-!minute!-!second!"
+set "source=%USERPROFILE%\AppData\Roaming\.minecraft\mods"
+set "destination=%~dp0"
+set "folderName=mods_!currentDate!"
+xcopy "%source%" "%destination%!folderName!" /e /i /y /h
+if exist "%~dp0\7z.exe" ("%~dp0\7z.exe" a "%destination%!folderName!.7z" "%destination%!folderName!\*")
