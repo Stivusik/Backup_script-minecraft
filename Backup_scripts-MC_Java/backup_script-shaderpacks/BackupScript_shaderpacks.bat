7750bcc341cf221cd5a6d40863c5dbc6d7da9d76
Stivusik <ferfersilver@gmail.com>
2025-03-23 08:50:24 +0200

    Update BackupScript_shaderpacks.bat

--- Backup_scripts-MC_Java/backup_script-shaderpacks/BackupScript_shaderpacks.bat
+++ Backup_scripts-MC_Java/backup_script-shaderpacks/BackupScript_shaderpacks.bat
@@ -11,5 +11,5 @@ set "currentDate=!day!-!month!-!year!_!hour!-!minute!-!second!"
 set "source=%USERPROFILE%\AppData\Roaming\.minecraft\shaderpacks"
 set "destination=%~dp0"
 set "folderName=shaderpacks_!currentDate!"
-xcopy "%source%" "%destination%!folderName!" /e /i /y /h
+xcopy "%source%" "%destination%!folderName!" /e /i /y /h /g
 if exist "%~dp0\7z.exe" ("%~dp0\7z.exe" a "%destination%!folderName!.7z" "%destination%!folderName!\*")
